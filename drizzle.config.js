32cb32f20aeb5ac09cc8eea52cacb59127facafa
Mehul Singh <mehulsingh076@gmail.com>
2025-04-21 20:58:54 +0530

    .

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,7 +5,6 @@ export default defineConfig({
   schema: './configs/schema.js',
   dialect: 'postgresql',
   dbCredentials: {
-    url: 'postgresql://neondb_owner:npg_fBmTFw6vs2Kr@ep-wild-wave-a410rfs4-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require
-'
+    url: 'postgresql://neondb_owner:npg_fBmTFw6vs2Kr@ep-wild-wave-a410rfs4-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require'
   },
 });
