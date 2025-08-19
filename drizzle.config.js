2843cb0fd3874d59e708a398bc6d3891ee9d49c3
Mehul Singh <mehulsingh076@gmail.com>
2025-08-19 11:05:23 +0530

    Update drizzle.config.js

--- drizzle.config.js
+++ drizzle.config.js
@@ -5,6 +5,6 @@ export default defineConfig({
   schema: './configs/schema.js',
   dialect: 'postgresql',
   dbCredentials: {
-    url: 'postgresql://neondb_owner:npg_fBmTFw6vs2Kr@ep-wild-wave-a410rfs4-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require'
+    url: `${process.env.NEXT_PUBLIC_DATABASE_CONNECTION_STRING}`
   },
 });
